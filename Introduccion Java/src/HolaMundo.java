5fc63ac1926040e84d80af55ee935b1cf04a3756
oscarfhdev <oscarfh.dev@gmail.com>
2025-06-06 23:59:59 +0200

    CREATE Introduccion Java & NeatBeans --> Hello World in IntelliJ and NetBeans

---
+++ Introduccion Java/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    // Agregamos el método principl (main)
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo con Java");
+        System.out.println("Saludos");
+    }
+}
---
+++ Introduccion Java/src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Óscar Fernández Hernández");
+        System.out.println("Edad: 18 años");
+        System.out.println("País: España");
+    }
+}
