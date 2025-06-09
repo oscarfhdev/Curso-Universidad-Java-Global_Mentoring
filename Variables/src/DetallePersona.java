92ea66335fd09bca9ba22b75d03e53dce4aee562
oscarfhdev <oscarfh.dev@gmail.com>
2025-06-09 11:52:51 +0200

    CREATE Variables/src/DetallePersona.java --> new class to use the variables

---
+++ Variables/src/DetallePersona.java
@@ -0,0 +1,17 @@
+public class DetallePersona {
+    public static void main(String[] args) {
+        // Declaramos las variables
+        String nombreCompleto = "Óscar Fernández";
+        int edad = 18;
+        double altura = 1.74;
+        String paisOrigen = "España";
+        char estado = 'S'; // C-Casado S-Soltero
+
+        // Imprimimos las variables
+        System.out.println(nombreCompleto);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(paisOrigen);
+        System.out.println(estado);
+    }
+}
