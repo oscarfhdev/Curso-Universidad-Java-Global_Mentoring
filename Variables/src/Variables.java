651fa2a2bab5d776e4f5b4437623e753b445536a
oscarfhdev <oscarfh.dev@gmail.com>
2025-06-09 11:00:11 +0200

    CREATE Variables --> new class

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -2,8 +2,8 @@ public class Variables {
     public static void main(String[] args) {
         // Variables
         int edad = 31;
-        double precio = 300.40;
-        boolean disponible = true; // true/false
+        double precio = 300.4;
+        boolean disponible = false; // true/false
         char genero;
         genero = 'M'; // M-Masculino / F-Femenino
 
@@ -12,9 +12,11 @@ public class Variables {
         // Modificar el valor
         edad = 35;
         System.out.println(edad);
+
         // Imprimir el resto de las variables
         System.out.println(precio);
         System.out.println(disponible);
         System.out.println(genero);
+
     }
 }
