3cbe83100abc335375f7a198b825c5b89847503b
oscarfhdev <oscarfh.dev@gmail.com>
2025-06-15 17:49:50 +0200

    CREATE Ciclos/src/CicloFor.java & NumerosParesCicloFor.java
    new class to practice the for loop

---
+++ Ciclos/src/NumerosParesCicloFor.java
@@ -0,0 +1,10 @@
+public class NumerosParesCicloFor {
+    public static void main(String[] args) {
+        System.out.println("*** Números Pares con Ciclo For ***");
+
+        for (int i = 0; i <= 20 ; i++) {
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+        }
+    }
+}
