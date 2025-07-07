5852327ee7623fc1cd03529ddcbfdaa26b8077f4
oscarfhdev <oscarfh.dev@gmail.com>
2025-07-07 23:26:08 +0200

    CREATE zona_fit/datos & zona_fit/dominio
    implemented new classes and performed a client listing test

---
+++ ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
@@ -0,0 +1,78 @@
+package zona_fit.datos;
+
+import zona_fit.conexion.Conexion;
+import zona_fit.dominio.Cliente;
+
+import java.rmi.server.ExportException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import static zona_fit.conexion.Conexion.getConexion;
+
+public class ClienteDAO implements IClienteDAO{
+    @Override
+    public List<Cliente> listarClientes() {
+        List<Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+        var sql = "SELECT * FROM cliente ORDER BY id";
+        try {
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()){
+                var cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                clientes.add(cliente);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error al listar clientes: " + e.getMessage());
+        }
+
+        finally {
+            try {
+                con.close();
+            }
+            catch (Exception e){
+                System.out.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
+        return clientes;
+    }
+
+    @Override
+    public boolean buscarClientePorId(Cliente cliente) {
+        return false;
+    }
+
+    @Override
+    public boolean agregarCliente(Cliente cliente) {
+        return false;
+    }
+
+    @Override
+    public boolean modificarCliente(Cliente cliente) {
+        return false;
+    }
+
+    @Override
+    public boolean eliminarCliente(Cliente cliente) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // Listar clientes
+        System.out.println("*** Listar Clientes ***");
+        var clienteDAO = new ClienteDAO();
+        var clientes = clienteDAO.listarClientes();
+        clientes.forEach(System.out::println);
+
+    }
+}
