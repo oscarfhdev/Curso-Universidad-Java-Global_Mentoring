c9303a6f71a681fa6eb1942f2fbfc2e772b44974
oscarfhdev <oscarfh.dev@gmail.com>
2025-07-10 18:46:14 +0200

    UPDATE ZonaFitSpring/.../ZonaFitApplication.java
    Implemented new methods to update, add and delete clients

--- ZonaFitSpring/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ ZonaFitSpring/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,16 +1,14 @@
 package gm.zona_fit;
 
 import gm.zona_fit.modelo.Cliente;
+import gm.zona_fit.servicio.ClienteServicio;
 import gm.zona_fit.servicio.IClienteServicio;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.awt.event.WindowFocusListener;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -66,6 +64,9 @@ public class ZonaFitApplication implements CommandLineRunner{
 		switch (opcion){
 			case 1 -> listarClientes();
 			case 2 -> buscarCliente(consola);
+			case 3 -> agregarCliente(consola);
+			case 4 -> actualizarCliente(consola);
+			case 5 -> eliminarCliente(consola);
 			case 6 -> {
 				logger.info("Saliendo del sistema...");
 				return true;
@@ -75,10 +76,12 @@ public class ZonaFitApplication implements CommandLineRunner{
 	}
 
 	private void listarClientes(){
+		System.out.println("--- Listar clientes ---");
 		clienteServicio.listarClientes().forEach(System.out::println);
 	}
 
 	private void buscarCliente(Scanner consola){
+		System.out.println("--- Buscar cliente ---");
 		System.out.print("Ingresa el id del cliente que deseas buscar: ");
 		var idABuscar = Integer.parseInt(consola.nextLine());
 
@@ -89,6 +92,58 @@ public class ZonaFitApplication implements CommandLineRunner{
 		}
 		else {
 			System.out.println("No se ha encontrado ningún cliente con id: " + idABuscar);
+		}
+	}
+
+	private void agregarCliente(Scanner consola){
+		System.out.print("Ingrese el nombre del nuevo cliente: ");
+		var nombre = consola.nextLine();
+		System.out.print("Ingrese el apellido del nuevo cliente: ");
+		var apelido = consola.nextLine();
+		System.out.print("Ingrese la membresía del nuevo cliente: ");
+		var membresia = Integer.parseInt(consola.nextLine());
+
+		var clienteAAgregar = new Cliente(null, nombre, apelido, membresia);
+
+		clienteServicio.guardarCliente(clienteAAgregar);
+		System.out.println("Cliente agregado corectamente: " + clienteAAgregar);
+	}
+
+	private void actualizarCliente(Scanner consola){
+		System.out.println("--- Actualizar cliente ---");
+		System.out.print("Ingresa el id del cliente a actualizar: ");
+		var idCliente = Integer.parseInt(consola.nextLine());
+
+		if (clienteServicio.buscarClientePorId(idCliente) != null){
+			System.out.print("Ingrese el nombre del cliente: ");
+			var nombre = consola.nextLine();
+			System.out.print("Ingrese el apellido del cliente: ");
+			var apelido = consola.nextLine();
+			System.out.print("Ingrese la membresía del cliente: ");
+			var membresia = Integer.parseInt(consola.nextLine());
+
+			clienteServicio.guardarCliente(new Cliente(idCliente, nombre, apelido, membresia));
+			System.out.println("Cliente actualizado correctamente");
+		}
+		else {
+			System.out.println("No se ha encontrado ningún cliente con id: " + idCliente);
+		}
+
+
+	}
+
+	private void eliminarCliente(Scanner consola){
+		System.out.println("--- Eliminar cliente ---");
+		System.out.print("Ingresa el id del cliente a eliminar: ");
+		var idCliente = Integer.parseInt(consola.nextLine());
+
+		var clienteABuscar = clienteServicio.buscarClientePorId(idCliente);
+		if ( clienteABuscar != null){
+			clienteServicio.eliminarCliente(clienteABuscar);
+			System.out.println("Cliente eliminado correctamente");
+		}
+		else {
+			System.out.println("No se ha encontrado ningún cliente con esta id: " + idCliente);
 		}
 	}
 }
