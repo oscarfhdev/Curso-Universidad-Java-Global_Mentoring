42d9a2aa9b986b959010055fa8ebf33eff3a30c8
oscarfhdev <oscarfh.dev@gmail.com>
2025-07-12 20:54:45 +0200

    UPDATE Swing/src/Forma/Forma.java & Forma.form
    testing actions/key listeners

--- Swing/src/main/java/Forma.java
+++ Swing/src/main/java/Forma.java
@@ -1,16 +1,36 @@
 import com.formdev.flatlaf.FlatDarculaLaf;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Forma extends JFrame{
-    private JPanel panel1;
+    private JPanel panelPrincipal;
+    private JTextField campoTexto;
+    private JLabel replicadorLabel;
 
     public Forma(){
         inicializarForma();
+//        campoTexto.addActionListener(e -> {
+//            replicarTexto();
+//         });
+        campoTexto.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                replicarTexto();
+            }
+        });
+    }
+
+    private void replicarTexto(){
+        this.replicadorLabel.setText(this.campoTexto.getText());
     }
 
     private void inicializarForma(){
-        setContentPane(panel1);
+        setContentPane(panelPrincipal);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600, 400);
         setLocationRelativeTo(null); // Centramos la ventana
