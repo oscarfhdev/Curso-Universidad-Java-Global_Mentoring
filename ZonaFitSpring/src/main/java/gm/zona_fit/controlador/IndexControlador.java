2026bb537fcb310e6a92ad15e43b42ae76237b26
oscarfhdev <oscarfh.dev@gmail.com>
2025-07-18 17:56:47 +0200

    UPDATE ZonaFitSpring/.../resources/application.properties & CREATE controladro/IndexControlador.java
    new controller to xhtml, off spring banner on console)

---
+++ ZonaFitSpring/src/main/java/gm/zona_fit/controlador/IndexControlador.java
@@ -0,0 +1,33 @@
+package gm.zona_fit.controlador;
+
+import gm.zona_fit.modelo.Cliente;
+import gm.zona_fit.servicio.IClienteServicio;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import lombok.Data;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import org.slf4j.Logger;
+
+@Component
+@Data
+@ViewScoped
+public class IndexControlador {
+
+    @Autowired
+    IClienteServicio clienteServicio;
+    private List<Cliente> clientes;
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @PostConstruct
+    public void init(){
+        cargarDatos();
+    }
+
+    public void cargarDatos(){
+        this.clientes = this.clienteServicio.listarClientes();
+        this.clientes.forEach(cliente -> logger.info(cliente.toString()));
+    }
+}
