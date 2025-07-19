3ae030f064a8ca29dbfe063c8fa77b1c215e683a
oscarfhdev <oscarfh.dev@gmail.com>
2025-07-19 23:36:56 +0200

    UPDATED tareas/.../controlador/IndexControlador.Java & resources/index.fxml & TareasApplication.Java & CREATED presentacion/SistemaTareasFx.java
    classes configured to display the first window

--- tareas/src/main/java/gm/tareas/controlador/IndexControlador.java
+++ tareas/src/main/java/gm/tareas/controlador/IndexControlador.java
@@ -1,10 +1,13 @@
 package gm.tareas.controlador;
 
 import javafx.fxml.Initializable;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+@Component
 public class IndexControlador implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
