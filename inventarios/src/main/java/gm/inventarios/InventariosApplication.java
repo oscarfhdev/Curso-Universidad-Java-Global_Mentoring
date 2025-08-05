f202768fb7f1f6569b24f1901b3edc053174766e
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-05 20:34:38 +0200

    UPDATE inventarios/.../pom.xml --> configured and tested lombok

--- inventarios/src/main/java/gm/inventarios/InventariosApplication.java
+++ inventarios/src/main/java/gm/inventarios/InventariosApplication.java
@@ -1,5 +1,6 @@
 package gm.inventarios;
 
+import gm.inventarios.modelo.Producto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,15 @@ public class InventariosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventariosApplication.class, args);
+
+		// Prueba de Lombok
+		Producto producto = new Producto();
+		producto.setDescripcion("Camisa M");
+		producto.setPrecio(600.0);
+		producto.setExistencia(30);
+
+		// Imprimir el objeto con ToString de Lombok
+		System.out.println(producto);
 	}
 
 }
