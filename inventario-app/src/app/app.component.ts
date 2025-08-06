d82010bd0801830f4fa26d28177e710e9aa73bd2
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-06 21:11:41 +0200

    CREATED inventario-app/.../src/app/producto-lista
    new component, now app.component.html shows producto-lista.component.html

--- inventario-app/src/app/app.component.ts
+++ inventario-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductoListaComponent } from "./producto-lista/producto-lista.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ProductoListaComponent]
 })
 export class AppComponent {
   title = 'inventario-app';
---
+++ inventario-app/src/app/producto-lista/producto-lista.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-lista',
+  imports: [],
+  templateUrl: './producto-lista.component.html',
+})
+export class ProductoListaComponent {
+
+}
