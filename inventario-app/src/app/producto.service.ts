4a8f9eaa936a376367c0fbecf168130120ae451c
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-07 23:47:21 +0200

    CREATED inventario-app/.../src/app/producto.service.ts
    new service producto & added a table content

---
+++ inventario-app/src/app/producto.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from './producto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+  private urlBase = "http://localhost:8080/inventario-app/productos";
+  private clienteHttp = inject(HttpClient)
+
+  obtenerProductosLista(): Observable<Producto[]>{
+    return this.clienteHttp.get<Producto[]>(this.urlBase);
+
+  }
+
+  constructor() { }
+}
