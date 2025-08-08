a3bec2e62967abd839e19c7c4a526082937a4942
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-08 23:24:48 +0200

    UPDATED inventario-app/...src/app/producto-lista
    updated .html & .ts, now we can view the inventory via web

--- inventario-app/src/app/producto-lista/producto-lista.component.ts
+++ inventario-app/src/app/producto-lista/producto-lista.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Producto } from '../producto';
+import { ProductoService } from '../producto.service';
 
 @Component({
   selector: 'app-producto-lista',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './producto-lista.component.html',
 })
 export class ProductoListaComponent {
+  productos!: Producto[];
+  private productoServicio = inject(ProductoService);
 
+  ngOnInit() {
+    // Cargar los productos
+    this.obtenerProductos();
+  }
+
+  private obtenerProductos(): void {
+    this.productoServicio.obtenerProductosLista().subscribe(
+      {
+        next: (datos) => {
+          this.productos = datos;
+        },
+        error: (error) => {
+          console.error("Error al obtener los productos", error)
+        }
+      }
+    );
+  }
 }
