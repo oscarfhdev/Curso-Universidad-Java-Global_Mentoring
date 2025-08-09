a7f3b813dbb5a21848eb5b8738fa48036012aaf7
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-09 21:51:32 +0200

    CREATED inventarios/.../excepcion/RecursoNoEncontradoExcepcion.java & UPDATED /controlador/ProductoControlador.java
    now we can search objects by id (postman)

--- inventarios/src/main/java/gm/inventarios/controlador/ProductoControlador.java
+++ inventarios/src/main/java/gm/inventarios/controlador/ProductoControlador.java
@@ -1,10 +1,12 @@
 package gm.inventarios.controlador;
 
+import gm.inventarios.excepcion.RecursoNoEncontradoExcepcion;
 import gm.inventarios.modelo.Producto;
 import gm.inventarios.servicio.ProductoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,5 +32,16 @@ public class ProductoControlador {
     public Producto agregarProducto(@RequestBody Producto producto){
         logger.info("Producto a agregar" + producto);
         return this.productoServicio.guardarProducto(producto);
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable int id){
+        Producto producto = productoServicio.buscarProductoPorId(id);
+        if(producto != null){
+            return ResponseEntity.ok(producto);
+        }
+        else {
+            throw new RecursoNoEncontradoExcepcion("No se encontró el producto con id: " + id);
+        }
     }
 }
