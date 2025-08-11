564c2e80630dff622c1fbbde5a7eaffeed1536c8
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-11 22:17:18 +0200

    UPDATED inventario-app/.../editar-producto.html&ts
    now we can update the products via web

--- inventario-app/src/app/producto.service.ts
+++ inventario-app/src/app/producto.service.ts
@@ -18,5 +18,13 @@ export class ProductoService {
   agregarProducto(producto: Producto): Observable<Object>{
     return this.clienteHttp.post(this.urlBase, producto);
   }
+
+  obtenerProductoPorId(id: number){
+    return this.clienteHttp.get<Producto>(`${this.urlBase}/${id}`)
+  }
+
+  editarProducto(id:number, producto: Producto){
+    return this.clienteHttp.put(`${this.urlBase}/${id}`, producto);
+  }
   constructor() { }
 }
