df93145948e4d03b580b46b115de25ada8c3efe0
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-15 20:25:10 +0200

    CREATED NeatBeans/IntroduccionJava project --> testing IDE

--- NetBeans/IntroduccionJava/src/HolaMundo.java
+++ NetBeans/IntroduccionJava/src/HolaMundo.java
@@ -75,10 +75,97 @@ public class HolaMundo {
         
         
         // Tipos primitivos de tipo flotante
-        float numeroFloat = (float) 10.0;
+        float numeroFloat = (float) 3.4028236E38D;
         System.out.println("numeroFloat = " + numeroFloat);
         System.out.println("Valor mínimo float:" + Float.MIN_VALUE);
         System.out.println("Valor màximo float:" + Float.MAX_VALUE);
+        
+        double numeroDouble = (double) 1.7976931348623157E08;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("Valor mínimo double:" + Double.MIN_VALUE);
+        System.out.println("Valor màximo double:" + Double.MAX_VALUE);
+        
+        
+        var numeroEntero2 = 10;
+        System.out.println("numeroEntero2 = " + numeroEntero2);
+        
+        var numeroDouble2 = 10.0;
+        System.out.println("numeroDouble2 = " + numeroDouble2);
+        
+        var numeroFloat2 = 10.0F;
+        System.out.println("numeroFloat2 = " + numeroFloat2);
+        
+        
+        char micaracter = 'a';
+        System.out.println("micaracter = " + micaracter);
+        
+        char varChar = '\u0021';
+        System.out.println("varChar = " + varChar);
+       
+        char varCharDecimal = 33;
+        System.out.println("varCharDecimal = " + varCharDecimal);
+        
+        char varCharSimbolo = '!';
+        System.out.println("varCharSimbolo = " + varCharSimbolo);
+        
+        var varChar2 = '\u0021';
+        System.out.println("varChar2 = " + varChar2);
+       
+        var varCharDecimal2 = 33;
+        System.out.println("varCharDecimal2 = " + varCharDecimal2);
+        
+        var varCharSimbolo2 = '!';
+        System.out.println("varCharSimbolo2 = " + varCharSimbolo2);
+        
+        int VariableEnteraSimbolo = '!';
+        System.out.println("VariableEnteraSimbolo = " + VariableEnteraSimbolo);
+        
+        int letra = 'A';
+        System.out.println("letra = " + letra);
+        
+        boolean varBoolean = false;
+        System.out.println("varBoolean = " + varBoolean);
+        
+        if (varBoolean) {
+            System.out.println("La variable es verdadera");
+        }
+        else{
+            System.out.println("La variable es falsa");
+        }
+        
+        
+        var edad = 30;
+        var esAdulto = edad>= 18;
+        if(esAdulto){
+            System.out.println("Eres mayor de edad");
+        }
+        else{
+            System.out.println("Eres menor de edad");
+        }
+        
+        
+        // Convertir tipo String a tipo int
+        var edad2 = "20";
+        // var edad = Integer.parseInt("20");
+        System.out.println("edad = " + (edad2 + 1));
+        
+        var valorPi = Double.parseDouble("3.1416");
+        System.out.println("valorPi = " + valorPi);
+        
+        // Pedir un valor
+          var consola = new Scanner(System.in);
+//        System.out.print("Proporciona tu edad:");
+//        edad = Integer.parseInt(consola.nextLine());
+//        System.out.println("consola = " + consola);
+        
+        var edadTexto = String.valueOf(edad);
+        System.out.println("edadTexto = " + edadTexto);
+        
+        var caracter = "hola".charAt(0);
+        System.out.println("caracter = " + caracter);
+        
+        System.out.print("Proporciona un carácter: " );
+        caracter = consola.nextLine().charAt(0);
+        System.out.println("caracter = " + caracter);
     }
-
 }
