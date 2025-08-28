d66defb7fccec0f8fed82a6d215b5bd35751d37d
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-28 23:37:52 +0200

    CREATED NetBeans/JDBC/IntroduccionJDBC project

---
+++ JDBC/ManejoJDBC/src/main/java/test/TestManejoUsuario.java
@@ -0,0 +1,26 @@
+package test;
+
+import datos.UsuarioDAO;
+import domain.Usuario;
+import java.util.List;
+
+public class TestManejoUsuario {
+    public static void main(String[] args) {
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        
+        List<Usuario> usuarios = usuarioDAO.seleccionar();
+//        usuarios.forEach(usuario -> System.out.println("usuario = " + usuario));
+        
+//        Usuario usuarioInsertar = new Usuario("alba", "albitaa");
+//        usuarioDAO.insertar(usuarioInsertar);
+        
+//        Usuario usuarioActualizar = new Usuario(4, "ventura", "venturita");
+//        usuarioDAO.actualizar(usuarioActualizar);
+        
+        Usuario usuarioEliminar = new Usuario(4);
+        usuarioDAO.eliminar(usuarioEliminar);
+
+        usuarios = usuarioDAO.seleccionar();
+        usuarios.forEach(usuario -> System.out.println("usuario = " + usuario));
+    }
+}
