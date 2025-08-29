2952df8f794495bba10dac93e376993702340133
oscarfhdev <oscarfh.dev@gmail.com>
2025-08-29 21:17:08 +0200

    CREATED NetBeans/JDBC/ManejoJDBC_Transacciones --> initialized project and estructured files

---
+++ NetBeans/JDBC/ManejoJDBC_Transacciones/src/main/java/test/TestManejoPersonas.java
@@ -0,0 +1,50 @@
+package test;
+
+import datos.PersonaJDBC;
+import domain.Persona;
+import java.util.List;
+
+public class TestManejoPersonas {
+    public static void main(String[] args) {
+        PersonaJDBC personaDAO = new PersonaJDBC();
+        List<Persona> personas = personaDAO.seleccionar();
+//        for (Persona persona : personas) {
+//            System.out.println("persona = " + persona);
+//        }
+        
+        
+//        System.out.println("\n\n");
+        // Hacemos la prueba para insertar un nuevo objeto persona
+//        Persona nuevaPersona = new Persona("Carlos", "Esperpento", "carlos@carlos.es", "834928");
+        // personaDAO.insertar(nuevaPersona);
+        
+//        personas = personaDAO.seleccionar();
+//        for (Persona persona : personas) {
+//            System.out.println("persona = " + persona);
+//        }
+//        
+//        System.out.println("\n\n");
+
+        
+        // Modificar un registro de persona existente
+//        Persona personaModificar = new Persona(4, "kero", "kero",  "kero", "kero");
+//        Persona personaModificar2 = new Persona(5, "kero2", "kero2",  "kero2", "kero2");
+//        personaDAO.actualizar(personaModificar);
+//        personaDAO.actualizar(personaModificar2);
+        
+        // Imprimimos de nuevo, no hacemos método para ahorrar tiempo
+//        personas = personaDAO.seleccionar();
+//        for (Persona persona : personas) {
+//            System.out.println("persona = " + persona);
+//        }
+        
+        Persona personaEliminar = new Persona(5);
+        personaDAO.eliminar(personaEliminar);
+
+        personas = personaDAO.seleccionar();
+        for (Persona persona : personas) {
+            System.out.println("persona = " + persona);
+        }
+        
+    }
+}
